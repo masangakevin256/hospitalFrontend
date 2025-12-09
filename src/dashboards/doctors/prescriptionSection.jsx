84217fd54b904ef34f57ddb66ab0e1780d875dbf
masangakevin256 <masangakevin1@gmail.com>
2025-12-09 10:00:05 +0300

    handling some errors

--- src/dashboards/doctors/prescriptionSection.jsx
+++ src/dashboards/doctors/prescriptionSection.jsx
@@ -393,7 +393,7 @@ function PrescriptionsSection({ doctorId }) {
   }
 
   return (
-    <div className="container-fluid py-4">
+    <div className="container-fluid py-4 section-container">
       {/* Header Section */}
       <div className="d-flex justify-content-between align-items-center mb-4">
         <div>
